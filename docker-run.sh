fcf8d04d4bb32ac53156ab2138185a98112a03ec
Filipe Oliveira <285038+filipeoliveiraa@users.noreply.github.com>
2023-05-24 11:10:22 +0100

    docker run script

---
+++ docker-run.sh
@@ -0,0 +1,19 @@
+#Clone repo
+FOLDER_SRC=$PWD
+echo $FOLDER_SRC
+git clone https://github.com/accelerat-ai/asterisk-unimrcp
+cd asterisk-unimrcp
+
+# Build docker images
+cd docker/build-ubuntu
+docker build -t asterisk-unimrcp-build:Asterisk-13.18.1 --build-arg ASTERISK_VER=13.18.1 --build-arg ASTERISK_MRCP_BRANCH=master .
+
+cd ../exec-ubuntu
+docker build -t asterisk-unimrcp-exec:Asterisk-13.18.1 --build-arg BASE_IMAGE=asterisk-unimrcp-build:Asterisk-13.18.1 .
+
+# Install unimrcp install
+docker run --rm -v $FOLDER_SRC/asterisk-unimrcp:/src --name asterisk-build asterisk-unimrcp-build:Asterisk-13.18.1 /src/install/make_asterisk_unimrcp_install.sh install_asterisk_unimrcp 13.18.1
+
+# Run test
+cd $FOLDER_SRC/docker/tests/
+docker-compose --profile test up
